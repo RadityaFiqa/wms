17202cd2a5bfdce5402f103fd9b546c7274c1445
Raditya Firman Syaputra <radityafiqa4@gmail.com>
2026-05-31 19:30:56 +0800

    WIP

--- apps/web/src/lib/axios.ts
+++ apps/web/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/auth';
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3002',
   headers: {
     'Content-Type': 'application/json',
   },
